2ab92ed09069f7f1e430c9901726c4c2b2b8df1f
Afam <afam262@gmail.com>
2020-12-10 00:47:53 -0600

    add questions and ds store file

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,50 @@
 const inquirer = require ('inquirer');
+const fs = require('fs')
 
 // array of questions for user
-// const questions = [
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?',
+      },
+      {
+        type: 'input',
+        name: 'location',
+        message: 'Where are you from?',
+      },
+         
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username',
+      },
+      {
+        type: 'input',
+        name: 'linkedin',
+        message: 'Enter your LinkedIn URL.',
+      },
 
-// ];
+];
+
+inquirer
+    .prompt(questions)
+    .then(answers => {
+        console.log(answers);
+    })
 
 // function to write README file
 // function writeToFile(fileName, data) {
+    
+
 // }
+// writeToFile(fileName, data);
+
+// fs.writeFile('readme.md', questions, (err) => {
+//     if (err) throw err;
+//     console.log('it worked')
+// })
+
 
 // function to initialize program
 // function init() {
